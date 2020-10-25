62cb0cb231c952ccbeb67d4f4445c82d037110e1
Victor M <victomc.586@gmail.com>
2020-10-24 19:37:34 -0500

    added required properties for authentication

--- Entities/ApplicationUser.cs
+++ Entities/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace BooksApi.Entities
 {
     public class ApplicationUser : IdentityUser
     {
-     
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
     }
 }
